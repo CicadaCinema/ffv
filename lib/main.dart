e73f18d986ca25d7d2b2c3fc1714705727011f61
CicadaCinema <52425971+CicadaCinema@users.noreply.github.com>
2022-03-13 16:32:57 +0000

    initial commit UI mostly done I have yet to implement hashing the chosen files

---
+++ lib/main.dart
@@ -0,0 +1,118 @@
+import 'dart:io';
+
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:cross_file/cross_file.dart';
+import 'package:desktop_drop/desktop_drop.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FluentApp(
+      title: 'Flutter Demo',
+      color: Colors.blue,
+      theme: ThemeData(
+        brightness: Brightness.light,
+      ),
+      home: ExampleDragTarget(),
+    );
+  }
+}
+
+class ExampleDragTarget extends StatefulWidget {
+  const ExampleDragTarget({Key? key}) : super(key: key);
+
+  @override
+  _ExampleDragTargetState createState() => _ExampleDragTargetState();
+}
+
+class _ExampleDragTargetState extends State<ExampleDragTarget> {
+  List<XFile> _fileList = [];
+  int _filesChosen = 0;
+
+  bool _dragging = false;
+  int _currentIndex = 0;
+  bool? _hashResult = null;
+
+  final TextStyle _bigStyle = const TextStyle(
+    fontSize: 64,
+    fontWeight: FontWeight.bold,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationView(
+      content: NavigationBody(index: _currentIndex, children: [
+        Container(
+          color: _dragging ? Colors.blue : Colors.white,
+          child: _filesChosen == 2
+              ? Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        _hashResult == null
+                            ? "Loading..."
+                            : _hashResult!
+                                ? "MATCH"
+                                : "NO MATCH",
+                        style: _bigStyle,
+                      ),
+                      Button(
+                        onPressed: () {
+                          _fileList.clear();
+                          _filesChosen = 0;
+                          setState(() {});
+                        },
+                        child: const Text("Refresh"),
+                      ),
+                    ],
+                  ),
+                )
+              : DropTarget(
+                  onDragDone: (detail) {
+                    setState(() {
+                      // hard exit if the user has dropped more than one file
+                      if (detail.files.length != 1) {
+                        exit(0);
+                      }
+                      // TODO: add support for multiple files
+                      // _list.addAll(detail.files);
+
+                      _fileList.add(detail.files[0]);
+                      _filesChosen += 1;
+                    });
+                  },
+                  onDragEntered: (detail) {
+                    setState(() {
+                      _dragging = true;
+                    });
+                  },
+                  onDragExited: (detail) {
+                    setState(() {
+                      _dragging = false;
+                    });
+                  },
+                  child: Container(
+                    child: Center(
+                      child: Text(
+                        (_filesChosen + 1).toString(),
+                        style: _bigStyle,
+                      ),
+                    ),
+                  ),
+                ),
+        ),
+        Center(
+          child: Text("Settings!"),
+        )
+      ]),
+    );
+  }
+}
